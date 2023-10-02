a236b80e64db7b13cd50658b2b1f3ce8dfdd072f
Hamidreza Bahramian <hamid.bahramian@gmail.com>
2023-10-01 20:31:36 -0400

    .

--- app/src/main/java/edu/iu/habahram/tasks/TaskItemAdapter.kt
+++ app/src/main/java/edu/iu/habahram/tasks/TaskItemAdapter.kt
@@ -2,42 +2,28 @@ package edu.iu.habahram.tasks
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import edu.iu.habahram.tasks.databinding.TaskItemBinding
 
 class TaskItemAdapter : ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
-
-
-    class TaskItemViewHolder(val rootView: CardView)
-        : RecyclerView.ViewHolder(rootView) {
-        val taskName = rootView.findViewById<TextView>(R.id.task_name)
-        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+            : TaskItemViewHolder = TaskItemViewHolder.inflateFrom(parent)
+    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+    class TaskItemViewHolder(val binding: TaskItemBinding)
+        : RecyclerView.ViewHolder(binding.root) {
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.task_item, parent, false)
-                as CardView
-                return TaskItemViewHolder(view)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
+                return TaskItemViewHolder(binding)
             }
         }
         fun bind(item: Task) {
-            taskName.text = item.taskName
-            taskDone.isChecked = item.taskDone
-             }
+            binding.task = item
         }
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder {
-        return TaskItemViewHolder.inflateFrom(parent)
-    }
-
-
-
-    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
-        val item =getItem(position)
-        holder.bind(item)
     }
 }
